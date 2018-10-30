9db97c762b8bbb388664c61d5fdfffaa853adcf5
Varun Garg <gargvarun15031998@gmail.com>
2018-10-31 01:15:34 +0530

    Update config to use json file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,4 +3,10 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://varun15@:varun@15@ds145563.mlab.com:45563/node_todo_api'
+};
+mongoose.connect(db.localhost || db.mlab);
+
 module.exports = {mongoose};
