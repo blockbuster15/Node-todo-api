5fe85cfc480b7114922d9370783b4888fe1510b2
Varun Garg <gargvarun15031998@gmail.com>
2018-11-26 21:51:53 +0530

    Correction in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://todoapi:nodetodoapi1@ds039351.mlab.com:39351/todo_api'
-};
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://todoapi:nodetodoapi1@ds039351.mlab.com:39351/todo_api'
+// };
+// mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
 module.exports = {mongoose};
